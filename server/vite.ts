db8bb049309ab3c458ac83d7c6fc15f8c96d93c0
chedli frini <chedlifrnigmail.com>
2025-09-08 00:04:33 +0200

    clean

--- server/vite.ts
+++ server/vite.ts
@@ -82,6 +82,6 @@ export function serveStatic(app: Express) {
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-  res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
